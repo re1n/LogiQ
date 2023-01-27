95292d5868c17452cd54e0e123b201fea82fb2f6
Josh Reiner <sc20jdr@leeds.ac.uk>
2023-01-27 22:59:15 +0000

    add only one logic

--- logic.py
+++ logic.py
@@ -232,6 +232,26 @@ def sentencesPets(picked_pets_and_people, pets, people, colours):
 		else:
 			sentence = f"¬∃x∃y {person}(x) → HasPet(x, y) ∧ {pet}(y)"
 			return (sentence, True)
+		
+	def somePersonColour(invert = False):
+		person = random.choice(people)
+		colour = random.choice(colours)
+		while colour == "NULL":
+			colour = random.choice(colours)
+		for pair in picked_pets_and_people:
+				if pair[0] == person and pair[2] == colour:
+					if not invert:
+						sentence = f"∃x∃y {person}(x) → HasPet(x, y) ∧ {colour}(y)"
+						return (sentence, True)
+					else:
+						sentence = f"¬∃x∃y {person}(x) → HasPet(x, y) ∧ {colour}(y)"
+						return (sentence, False)
+		if not invert:
+			sentence = f"∃x∃y {person}(x) → HasPet(x, y) ∧ {colour}(y)"
+			return (sentence, False)
+		else:
+			sentence = f"¬∃x∃y {person}(x) → HasPet(x, y) ∧ {colour}(y)"
+			return (sentence, True)
 	
 	def somePetPerson(invert = False):
 		person = random.choice(people)
@@ -250,10 +270,37 @@ def sentencesPets(picked_pets_and_people, pets, people, colours):
 		else:
 			sentence = f"¬∃x∃y {pet}(x) → HasOwner(x, y) ∧ {person}(y)"
 			return (sentence, True)
+		
+	def onlyOne(invert = False):
+		with_pets = without_pets = picked_pets_and_people
+		for pair in with_pets:
+			if pair[1] == "NULL":
+				with_pets.remove(pair)
+		for pair in without_pets:
+			if pair[1] != "NULL":
+				without_pets.remove(pair)
+		if len(with_pets) == 1:
+			# Exactly one person has a pet
+			if not invert:
+				sentence = f"∃x∃y HasPet(x, y) ∧ ∀w∃z HasPet(w, z) → x==w"
+				return (sentence, True)
+		else:
+			if not invert:
+				sentence = f"∃x∃y HasPet(x, y) ∧ ∀w∃z HasPet(w, z) → x==w"
+				return (sentence, False)
+		if len(without_pets) == 1:
+			# Exactly one person does not have a pet
+			if invert:
+				sentence = f"∃x∃y ¬HasPet(x, y) ∧ ∀w∃z (¬x==w) → HasPet(w, z)"
+				return (sentence, True)
+		else:
+			if invert:
+				sentence = f"∃x∃y ¬HasPet(x, y) ∧ ∀w∃z (¬x==w) → HasPet(w, z)"
+				return (sentence, False)
 
 	func_list = [everyone, everyoneColour, everyonePet, everyPersonColour, everyPersonPet,
-	    everyPersonPetColour, someone, someoneColour, someonePet, somePersonPet, everyPetPerson,
-		somePetPerson, somePersonPetColour]
+	    everyPetPerson, everyPersonPetColour, someone, someoneColour, someonePet, somePersonPet,
+		somePersonColour, somePetPerson, somePersonPetColour, onlyOne]
 	sentences = []
 	while len(sentences) < 4:
 		sentence = random.choice(func_list)(invert = random.choice([True, False]))
